1ba773853c4ecb7d2b377b7f1846f9c608e2c287
PaschalPM <okaforpaschal018@gmail.com>
2023-09-10 23:18:46 +0100

    Refactored codebase

--- src/lib/response_body.js
+++ src/lib/response_body.js
@@ -1,9 +1,18 @@
 const getCurrentDay = require("./current_day")
 
+const getISODate = () => {
+    let isoDate = (new Date()).toISOString()
+    let splitDate = isoDate.split('.')
+    let tail = splitDate.at(-1)
+    let lastChar = tail.slice(-1)
+    splitDate.pop()
+
+    return splitDate.join('')+lastChar
+}
 const responseBody = (slack_name = "PaschalPM", track = "Backend")=> ({
     slack_name,
     current_day: getCurrentDay(),
-    utc_time: (new Date()).toISOString(),
+    utc_time: getISODate(),
     track,
     github_file_url: "https://github.com/PaschalPM/hng-backend_task1/blob/main/api/index.js",
     github_repo_url: "https://github.com/PaschalPM/hng-backend_task1",
